2a21831c207112c66d56b422dabfd8507e8de985
timothy-tyk <timothy.tan@thoughtworks.com>
2026-08-11 10:47:18 +0800

    added engine files

--- java-limit-order-book/src/main/java/core/OrderBook.java
+++ java-limit-order-book/src/main/java/core/OrderBook.java
@@ -1,4 +1,10 @@
 package core;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class OrderBook {
+    private TreeMap<Long, PriceLevel> bids;
+    private TreeMap<Long, PriceLevel> asks;
+    private Map<Long, Order> ordersById;
 }
--- java-limit-order-book/src/main/java/core/PriceLevel.java
+++ java-limit-order-book/src/main/java/core/PriceLevel.java
@@ -1,4 +1,9 @@
 package core;
 
+import java.util.ArrayDeque;
+
 public class PriceLevel {
+    private ArrayDeque<Order> orders;
+    private long totalQuantity;
+    private long orderCount;
 }
